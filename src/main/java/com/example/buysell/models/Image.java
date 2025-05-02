924484b8eab75d68974cdb18fffc032c365c7535
apon0-0 <ayaulymtleuzhan777@gmail.com>
2025-05-02 18:03:34 +0500

    4done

---
+++ src/main/java/com/example/buysell/models/Image.java
@@ -0,0 +1,32 @@
+package com.example.buysell.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "images")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "original_file_name")
+    private String originalFileName;
+    @Column(name = "size")
+    private Long size;
+    @Column(name = "content_type")
+    private String contentType;
+    @Column(name = "is_preview_image")
+    private boolean isPreviewImage;
+    @Lob
+    private byte[] bytes;
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private Product product;
+}
--- src/main/java/com/example/buysell/models/Product.java
+++ src/main/java/com/example/buysell/models/Product.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity // Declares this class as a JPA entity
 @Table(name = "products") // Maps to the "products" table in the database
 @Data
@@ -25,4 +29,23 @@ public class Product {
     private String city;
     @Column(name = "author")
     private String author;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
+    private List<Image> images = new ArrayList<>();
+    private Long previewImageId;
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init() {
+        dateOfCreated = LocalDateTime.now();
+    }
+
+    public void addImageToProduct(Image image) {
+        image.setProduct(this);
+        images.add(image);
+    }
+
+
+
+
 }
