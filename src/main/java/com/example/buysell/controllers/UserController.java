bb0169a3a5ab38fa65f50eefe41b0bba4151ab40
apon0-0 <ayaulymtleuzhan777@gmail.com>
2025-05-05 01:19:26 +0500

    7d

--- src/main/java/com/example/buysell/controllers/UserController.java
+++ src/main/java/com/example/buysell/controllers/UserController.java
@@ -5,7 +5,9 @@ import com.example.buysell.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
 
 @Controller
 @RequiredArgsConstructor
@@ -29,9 +31,13 @@ public class UserController {
         return "redirect:/login";
     }
 
-    @GetMapping("/hello")
-    public String securityUrl() {
-        return "hello";
+    @GetMapping("/user/{user}")
+    public String userInfo(@PathVariable("user") User user, Model model) {
+        model.addAttribute("user", user);
+        model.addAttribute("products", user.getProducts());
+        return "user-info";
     }
+
+
 
 }
