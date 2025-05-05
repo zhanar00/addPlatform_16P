26b8da9b8f118b573d7998a43b3648857b23e1cc
apon0-0 <ayaulymtleuzhan777@gmail.com>
2025-05-05 17:57:13 +0500

    delnot1

--- src/main/java/com/example/buysell/controllers/UserController.java
+++ src/main/java/com/example/buysell/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.example.buysell.controllers;
 
-import ch.qos.logback.core.model.Model;
 import com.example.buysell.models.User;
 import com.example.buysell.services.UserService;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
 
+
 import java.security.Principal;
 
 @Controller // Marks this class as a Spring MVC controller
@@ -18,14 +18,9 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/login")
-<<<<<<< HEAD
-    public String login() {
-        return "login"; // Returns login page
-=======
     public String login(Principal principal, Model model) {
         model.addAttribute("user", userService.getUserByPrincipal(principal));
         return "login";
->>>>>>> 208c087f65538056c0602ba2aeecc5b2fef8aacc
     }
 
     @GetMapping("/profile")
@@ -37,27 +32,12 @@ public class UserController {
     }
 
     @GetMapping("/registration")
-<<<<<<< HEAD
-    public String registration() {
-        return "registration"; // Returns registration page
-=======
     public String registration(Principal principal, Model model) {
         model.addAttribute("user", userService.getUserByPrincipal(principal));
         return "registration";
->>>>>>> 208c087f65538056c0602ba2aeecc5b2fef8aacc
     }
 
     @PostMapping("/registration")
-<<<<<<< HEAD
-    public String createUser(User user) {
-        userService.createUser(user); // Registers new user
-        return "redirect:/login"; // Redirects to login after successful registration
-    }
-
-    @GetMapping("/hello")
-    public String securityUrl() {
-        return "hello"; // Simple test or secured page
-=======
     public String createUser(User user, Model model) {
         if (!userService.createUser(user)) {
             model.addAttribute("errorMessage", "Пользователь с email: " + user.getEmail() + " уже существует");
@@ -72,6 +52,5 @@ public class UserController {
         model.addAttribute("userByPrincipal", userService.getUserByPrincipal(principal));
         model.addAttribute("products", user.getProducts());
         return "user-info";
->>>>>>> 208c087f65538056c0602ba2aeecc5b2fef8aacc
     }
 }
